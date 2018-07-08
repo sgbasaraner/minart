68daa0e9d02b6fef5ee4d81f5274e4a5b608c246
Sarp Basaraner <sarpbasaraner@gmail.com>
2018-07-08 12:50:09 +0300

    add some error checking

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,27 @@ use image::{FilterType,
             PNG};
 
 fn main() {
+    let usage = "Usage: minart imagefile.png";
+
+    // Check for correct usage
     let file = if env::args().count() == 2 {
         env::args().nth(1).unwrap()
     } else {
-        panic!("Please enter a file")
+        panic!(usage)
     };
 
-    // TODO: Check for unspecified file types
-
-    let img = image::open(&Path::new(&file)).unwrap();
-
-    let file_splitted = file.split(".");
+    // Read file properties
+    let file_splitted: Vec<String> = file.split(".").map(|s| s.to_string()).collect();;
+    if file_splitted.len() < 2 {
+        panic!("File type must be explicitly specified.");
+    }
     let file_type = file_splitted.last().unwrap();
     let file_name = file.get(0..(file.chars().count() - 1 - file_type.chars().count())).unwrap();
+
+    let img = match image::open(&Path::new(&file)) {
+        Ok(i) => i,
+        Err(_) => panic!("No such file."),
+    };
 
     let dim_3x = img.dimensions();
     let dim_1x = (dim_3x.0 / 3, dim_3x.1 / 3);
