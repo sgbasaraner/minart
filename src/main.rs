8f787b4126efb35fcd2947ea19c452fecba1cfc3
Sarp Basaraner <sarpbasaraner@gmail.com>
2018-07-08 08:23:28 +0300

    print dimensions of image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate image;
+
+use std::env;
+use std::path::Path;
+
+use image::GenericImage;
+
 fn main() {
-    println!("Hello, world!");
+    let file = if env::args().count() == 2 {
+        env::args().nth(1).unwrap()
+    } else {
+        panic!("Please enter a file")
+    };
+
+    let img = image::open(&Path::new(&file)).unwrap();
+
+    println!("dimensions {:?}", img.dimensions());
 }
