ce34bc2a0c83247e39c03afdb2bb38f6e70b3738
Sarp Basaraner <sarpbasaraner@gmail.com>
2018-07-08 09:26:42 +0300

    determine resize dimensions

--- src/main.rs
+++ src/main.rs
@@ -14,5 +14,11 @@ fn main() {
 
     let img = image::open(&Path::new(&file)).unwrap();
 
-    println!("dimensions {:?}", img.dimensions());
+    let dim_3x = img.dimensions();
+    let dim_1x = (dim_3x.0 / 3, dim_3x.1 / 3);
+    let dim_2x = (dim_1x.0 * 2, dim_1x.1 * 2);
+
+    println!("3x: {:?}", dim_3x);
+    println!("3x: {:?}", dim_2x);
+    println!("3x: {:?}", dim_1x);
 }
